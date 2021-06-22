fdad936c75c9e7c3e1553c07e8c64dff8f0d2e74
NestleCafe <watson_xian@foxmail.com>
2021-06-22 21:22:47 +0800

    导航栏原理

---
+++ src/views/Count.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Count.Vue
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Labels.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Labels.vue</div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
