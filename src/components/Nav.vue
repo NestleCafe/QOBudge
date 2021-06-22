aeaa1b33b64cf60c91d4ce5e00a1043dbcd9c0ef
NestleCafe <watson_xian@foxmail.com>
2021-06-23 01:25:18 +0800

    创建了icon组件

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,24 +1,34 @@
 <template>
   <div class="nav">
-    <router-link to="/labels">标签</router-link>
+    <router-link to="/labels">
+      <icon name="Label"/>
+      标签</router-link
+    >
     |
-    <router-link to="/count">记一笔</router-link>
+    <router-link to="/count"> 
+      <icon name="add"/>
+      记一笔 
+    </router-link>
     |
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/statistics">
+    <icon name="data"/>
+    统计
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
+
+
 export default {
   name: "Nav",
 };
 </script>
 
 <style lang="scss" scoped>
-.nav{
+.nav {
   border: 1px solid red;
   display: flex;
   justify-content: center;
-
 }
 </style>
