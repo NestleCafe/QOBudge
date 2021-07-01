d891770f548317ccd9ee2e207592806794ed34a2
NestleCafe <watson_xian@foxmail.com>
2021-07-01 17:41:26 +0800

    封装window.createTag

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -39,16 +39,8 @@ export default class Lables extends Vue {
 
   createTag() {
     const name = window.prompt("请输入标签名");
-    if (name === "") {
-      window.alert("标签名不能为空！");
-      return;
-    } else if (name) {
-      const message = tagListModel.create(name);
-      if (message === "duplicated") {
-        window.alert("标签名重复！");
-      } else if (message === "success") {
-        window.alert("添加成功");
-      }
+    if (name) {
+      window.createTag(name);
     }
   }
 }
