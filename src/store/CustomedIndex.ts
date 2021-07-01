20c9eee5f8f61c38040dcd13e2fe9ed989037b3d
NestleCafe <watson_xian@foxmail.com>
2021-07-01 21:11:03 +0800

    将Models融合进store、模块化了store；删除了Models

--- src/store/CustomedIndex.ts
+++ src/store/CustomedIndex.ts
@@ -1,35 +1,9 @@
-import { tagListModel } from "@/models/tagListModel";
-import { recordListModel } from "@/models/recordListModel";
+import recordStore from "./recordStore";
+import tagStore from "./tagStore";
 
 const store = {
-  //record store
-  recordList: recordListModel.fetch(),
-  createRecord: (record: RecordItem) => {
-    recordListModel.create(record);
-  },
-
-  //tag store
-  tagList: tagListModel.fetch(),
-  createTag: (name: string) => {
-    const message = tagListModel.create(name);
-    if (name === "") {
-      window.alert("标签名不能为空！");
-      return;
-    } else if (message === "duplicated") {
-      window.alert("标签名重复！");
-    } else if (message === "success") {
-      window.alert("添加成功");
-    }
-  },
-  removeTag: (id: string) => {
-    return tagListModel.remove(id); //remove本身就是返回布尔
-  },
-  updateTag: (id: string, name: string) => {
-    return tagListModel.update(id, name);
-  },
-  findTag(id: string) {
-    return this.tagList.filter((t) => t.id === id)[0]; //filter返回的是数组
-  },
+  ...recordStore,
+  ...tagStore,
 };
 
 export default store;
